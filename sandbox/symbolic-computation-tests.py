18ff760fc4861ddcdd20740a57e62fa1c624de23
Yuval Bernard <yuval.bernard@weizmann.ac.il>
2024-12-12 23:29:51 +0200

    .

--- sandbox/symbolic-computation-tests.py
+++ sandbox/symbolic-computation-tests.py
@@ -32,25 +32,27 @@ def solve_bloch_mcconnell():
 
     # Define non-homogeneous part of BM-equations
     b = sp.Matrix([0, 0, R1a, 0, 0, R1b * f])
+    cos2_theta = (B0*gamma*(offset - dwa))**2 / ((gamma*power)**2 + (B0*gamma*(offset - dwa))**2)
     # theta = sp.atan(power/(offset*B0))
     # lambda_eff = (R1a * sp.cos(theta) + R2a * sp.sin(theta)).simplify()
-    method = "bird"
-    A_perturbed = A.subs(k, 0)
+    # method = "bird"
+    # A_perturbed = A.subs(k, 0)
     # coeffs = list(reversed(A_perturbed.charpoly().coeffs()))
     # lambda_eff = -6 * coeffs[0] / (coeffs[1] + sp.sqrt(25 * coeffs[1] ** 2 - 60 * coeffs[0] * coeffs[2]))
-    lambda_eff = A_perturbed.det(method) / A_perturbed.adjugate(method).trace()
-    A_rescaled = A - sp.eye(6) * lambda_eff
+    # lambda_eff = A_perturbed.det(method) / A_perturbed.adjugate(method).trace()
+    # A_rescaled = A - sp.eye(6) * lambda_eff
 
-    lambda_1 = A_rescaled.det(method) / A_rescaled.adjugate(method).trace() + lambda_eff
+    # lambda_1 = A_rescaled.det(method) / A_rescaled.adjugate(method).trace() + lambda_eff
     v1 = sp.Matrix([power * gamma, 0, offset * B0 * gamma]).normalized()
     # Calculate steady-state solution
     Z_ss = A.LUsolve(-b)[2]
+    R1p = R1a * cos2_theta / Z_ss
     # Simulate spectrum according to Zaiss 2013, 10.1002/nbm.2887
-    Z = (v1[2] ** 2 - Z_ss) * sp.exp(lambda_1 * tp) + Z_ss
+    Z = (v1[2] ** 2 - Z_ss) * sp.exp(-R1p * tp) + Z_ss
 
     gen_eigenvalue = sp.lambdify(
         [R1a, R2a, dwa, R1b, R2b, k, f, dwb, offset, power, B0, gamma],
-        lambda_1,
+        -R1p,
         modules="jax",
         cse=True,
         docstring_limit=0,
@@ -63,14 +65,14 @@ def solve_bloch_mcconnell():
         # cse=lambda expr: sp.cse(expr, order="none", optimizations="basic", list=False),
         docstring_limit=0,
     )
-    with open("src/solve_bloch_mcconnell.py", "w") as text_file:
-        text_file.write("import jax.numpy as jnp\n")
-        text_file.write(inspect.getsource(gen_spectrum))
+    # with open("src/solve_bloch_mcconnell.py", "w") as text_file:
+    #     text_file.write("import jax.numpy as jnp\n")
+    #     text_file.write(inspect.getsource(gen_spectrum))
 
-    filereplace("src/solve_bloch_mcconnell.py", "_lambdifygenerated", "gen_spectrum_symbolic")
-    filereplace("src/solve_bloch_mcconnell.py", "exp", "jnp.exp")
-    filereplace("src/solve_bloch_mcconnell.py", "abs", "jnp.absolute")
-    filereplace("src/solve_bloch_mcconnell.py", "sqrt", "jnp.sqrt")
+    # filereplace("src/solve_bloch_mcconnell.py", "_lambdifygenerated", "gen_spectrum_symbolic")
+    # filereplace("src/solve_bloch_mcconnell.py", "exp", "jnp.exp")
+    # filereplace("src/solve_bloch_mcconnell.py", "abs", "jnp.absolute")
+    # filereplace("src/solve_bloch_mcconnell.py", "sqrt", "jnp.sqrt")
     return gen_spectrum, gen_eigenvalue
 
 
@@ -157,33 +159,74 @@ def batch_gen_spectrum_analytical(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offset, po
     Z = (jnp.cos(theta) ** 2 - Z_ss) * jnp.exp(-R1p * tp) + Z_ss
     return Z
 
+@partial(jnp.vectorize, excluded=[0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11], signature="()->(k)")  # powers
+@partial(jnp.vectorize, excluded=[0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11], signature="()->()")  # offsets
+def batch_gen_eigenvalue_analytical(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offset, power, B0, gamma):
+    dwa *= B0 * gamma
+    dwb *= B0 * gamma
+    offset *= B0 * gamma
+    power *= gamma
+    theta = jnp.arctan(power / (offset - dwa))
+    R_eff = R1a * jnp.cos(theta) ** 2 + R2a * jnp.sin(theta) ** 2
+    REXMAX = ((k * f * power**2) / ((offset - dwa) ** 2 + power**2)) * (
+        (dwb) ** 2 + ((offset - dwa) ** 2 + power**2) * R2b / k + R2b * (k + R2b)
+    )
+    GAMMA = 2 * jnp.sqrt((k + R2b) / k * power**2 + (k + R2b) ** 2)
+    R_ex = REXMAX / ((GAMMA / 2) ** 2 + (offset - dwb) ** 2)
+    R1p = R_eff + R_ex
+    return -R1p
 
-# R1a = 0.33
-# R2a = 0.5
-# dwa = 0.0
-# R1b = 1.0
-# R2b = 30.0
-# k = 200.0
-# f = 5e-4
-# dwb = 3.5
-# offsets = jnp.arange(-6, 6, 0.01, dtype=float)
-# powers = jnp.array([1.0, 3.0], dtype=float)
-# B0 = 4.7
-# gamma = 267.522
-# tp = 15.0
 
-# Z_symbolic = batch_gen_spectrum_symbolic(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma, tp)
-# Z_numerical = batch_gen_spectrum_numerical(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma, tp)
-# Z_analytical = batch_gen_spectrum_analytical(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma, tp)
-# # lambda_1_symbolic = batch_gen_eigenvalue_symbolic(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma)
-# # lambda_1_numerical = batch_gen_eigenvalue_numerical(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma)
+R1a = 0.33
+R2a = 0.5
+dwa = 0.0
+R1b = 1.0
+R2b = 30.0
+k = 200.0
+f = 5e-4
+dwb = 3.5
+offsets = jnp.arange(-6, 6, 0.01, dtype=float)
+powers = jnp.array([1.0, 3.0], dtype=float)
+B0 = 4.7
+gamma = 267.522
+tp = 15.0
 
-# fig, axs = plt.subplots(ncols=3)
-# fig.set_figwidth(14)
-# axs[0].plot(offsets, Z_symbolic.T)
-# axs[1].plot(offsets, Z_numerical.T)
-# axs[2].plot(offsets, Z_analytical.T)
-# axs[0].set_title("Symbolic")
-# axs[1].set_title("Numerical")
-# axs[2].set_title("Analytical")
-# fig.show()
+Z_symbolic = batch_gen_spectrum_symbolic(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma, tp)
+Z_numerical = batch_gen_spectrum_numerical(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma, tp)
+Z_analytical = batch_gen_spectrum_analytical(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma, tp)
+lambda_1_symbolic = batch_gen_eigenvalue_symbolic(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma)
+lambda_1_numerical = batch_gen_eigenvalue_numerical(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma)
+lambda_1_analytical = batch_gen_eigenvalue_analytical(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offsets, powers, B0, gamma)
+
+
+fig, axs = plt.subplots(ncols=3)
+fig.set_figwidth(14)
+axs[0].plot(offsets, lambda_1_symbolic.T)
+axs[1].plot(offsets, lambda_1_numerical.T)
+axs[2].plot(offsets, lambda_1_analytical.T)
+axs[0].set_title("Symbolic")
+axs[1].set_title("Numerical")
+axs[2].set_title("Analytical")
+fig.show()
+
+jnp.linalg.norm(lambda_1_symbolic.flatten() - lambda_1_numerical.flatten())
+
+# %%
+@jax.jit
+def gen_Z_ss(R1a, R2a, dwa, R1b, R2b, k, f, dwb, offset, power, B0, gamma, tp):
+    dwa *= B0 * gamma
+    dwb *= B0 * gamma
+    offset *= B0 * gamma
+    power *= gamma
+    theta = jnp.arctan(power / (offset - dwa))
+    R_eff = R1a * jnp.cos(theta) ** 2 + R2a * jnp.sin(theta) ** 2
+    REXMAX = ((k * f * power**2) / ((offset - dwa) ** 2 + power**2)) * (
+        (dwb) ** 2 + ((offset - dwa) ** 2 + power**2) * R2b / k + R2b * (k + R2b)
+    )
+    GAMMA = 2 * jnp.sqrt((k + R2b) / k * power**2 + (k + R2b) ** 2)
+    R_ex = REXMAX / ((GAMMA / 2) ** 2 + (offset - dwb) ** 2)
+    R1p = R_eff + R_ex
+    Z_ss = jnp.cos(theta) ** 2 * R1a / R1p
+    return Z_ss
+
+gen_Z_ss(R1a, R2a, dwa, R1b, R2b, k, f, dwb, 0, 1, B0, gamma, tp)
--- sandbox/symbolic_computation-tests-3-pools.py
+++ sandbox/symbolic_computation-tests-3-pools.py
@@ -31,7 +31,9 @@ def solve_bloch_mcconnell_3_pools():
         dwc,
         power,
         tp,
-    ) = sp.symbols("R1a, R2a, R1b, R2b, R1c, R2c,  kb, kc,  fb, fc,  offset, dwa, dwb, dwc,  power, tp", real=True)
+        B0,
+        gamma
+    ) = sp.symbols("R1a, R2a, R1b, R2b, R1c, R2c,  kb, kc,  fb, fc,  offset, dwa, dwb, dwc,  power, tp, B0, gamma", real=True)
     kab = fb * kb
     kac = fc * kc
     L_a = sp.Matrix(
@@ -94,6 +96,8 @@ def solve_bloch_mcconnell_3_pools():
             dwb,
             dwc,
             power,
+            B0,
+            gamma
         ],
         lambda_1,
         modules="jax",
@@ -118,99 +122,100 @@ def solve_bloch_mcconnell_3_pools():
             dwc,
             power,
             tp,
+            B0,
+            gamma
         ],
         Z,
         modules="jax",
         cse=True,
         docstring_limit=0,
     )
-    with open("../lib/solve_bloch_mcconnell_3_pools.py", "w") as text_file:
+    with open("solve_bloch_mcconnell_3_pools.py", "w") as text_file:
         text_file.write("import jax.numpy as jnp\n")
         text_file.write(inspect.getsource(gen_spectrum))
 
-    filereplace("../lib/solve_bloch_mcconnell_3_pools.py", "_lambdifygenerated", "gen_spectrum_symbolic")
-    filereplace("../lib/solve_bloch_mcconnell_3_pools.py", "exp", "jnp.exp")
-    filereplace("../lib/solve_bloch_mcconnell_3_pools.py", "abs", "jnp.absolute")
-    filereplace("../lib/solve_bloch_mcconnell_3_pools.py", "sqrt", "jnp.sqrt")
+    filereplace("solve_bloch_mcconnell_3_pools.py", "_lambdifygenerated", "gen_spectrum_symbolic")
+    filereplace("solve_bloch_mcconnell_3_pools.py", "exp", "jnp.exp")
+    filereplace("solve_bloch_mcconnell_3_pools.py", "abs", "jnp.absolute")
+    filereplace("solve_bloch_mcconnell_3_pools.py", "sqrt", "jnp.sqrt")
     return gen_spectrum, gen_eigenvalue
 
 
 gen_spectrum_symbolic, gen_eigenvalue_symbolic = solve_bloch_mcconnell_3_pools()
 
-# %% Check eigenvalue and Z-spectrum validity
-import sys
+# # %% Check eigenvalue and Z-spectrum validity
+# import sys
 
-sys.path.append("../lib")
-from functools import partial
+# from functools import partial
 
-from solve_bloch_mcconnell_3_pools import gen_spectrum_symbolic
-
-
-@jax.jit
-@partial(jnp.vectorize, excluded=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15], signature="()->(k)")  # powers
-def batch_gen_spectrum_symbolic(R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offset, dwa, dwb, dwc, power, tp):
-    return gen_spectrum_symbolic(R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offset, dwa, dwb, dwc, power, tp)
+# from solve_bloch_mcconnell_3_pools import gen_spectrum_symbolic
 
 
-@jax.jit
-@partial(jnp.vectorize, excluded=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15], signature="()->(k)")  # powers
-@partial(jnp.vectorize, excluded=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15], signature="()->()")  # offsets
-def batch_gen_spectrum_numerical(R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offset, dwa, dwb, dwc, power, tp):
-    dwa *= B0 * gamma
-    dwb *= B0 * gamma
-    dwc *= B0 * gamma
-    offset *= B0 * gamma
-    power *= gamma
-
-    kab = fb * kb
-    kac = fc * kc
-    L_a = jnp.array([[-R2a, dwa - offset, 0], [offset - dwa, -R2a, power], [0, -power, -R1a]])
-    L_b = jnp.array([[-R2b, dwb - offset, 0], [offset - dwb, -R2b, power], [0, -power, -R1b]])
-    L_c = jnp.array([[-R2c, dwc - offset, 0], [offset - dwc, -R2c, power], [0, -power, -R1c]])
-    K = jnp.array(
-        [
-            [-(kab + kac), kb, kc],
-            [kab, -kb, 0],
-            [kac, 0, -kc],
-        ]
-    )
-    # Define coefficient matrix of BM equations
-    A = jax.scipy.linalg.block_diag(L_a, L_b, L_c) + jnp.linalg.tensordot(K, jnp.eye(3))
-    b = jnp.array([0, 0, R1a, 0, 0, R1b * fb, 0, 0, R1c * fc])
-
-    M0 = jnp.array([0, 0, 1, 0, 0, fb, 0, 0, fc])
-    M_ss = jnp.linalg.solve(A, -b)
-    Z = ((M0 - M_ss) @ jax.scipy.linalg.expm(A * tp, max_squarings=18) + M_ss)[2]
-    return Z
+# @jax.jit
+# @partial(jnp.vectorize, excluded=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15], signature="()->(k)")  # powers
+# def batch_gen_spectrum_symbolic(R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offset, dwa, dwb, dwc, power, tp):
+#     return gen_spectrum_symbolic(R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offset, dwa, dwb, dwc, power, tp)
 
 
-R1a = 1 / 3.0
-R2a = 1 / 2.0
-dwa = 0.0
-R1b = 1.0
-R2b = 30.0
-R1c = 2.0
-R2c = 15.0
-kb = 200.0
-kc = 30.0
-fb = 5e-4
-fc = 1e-3
-dwb = 3.5
-dwc = 2.0
-offsets = jnp.arange(-6, 6, 0.01, dtype=float)
-powers = jnp.array([1.0, 3.0], dtype=float)
-B0 = 4.7
-gamma = 267.522
-tp = 15.0
+# @jax.jit
+# @partial(jnp.vectorize, excluded=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15], signature="()->(k)")  # powers
+# @partial(jnp.vectorize, excluded=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15], signature="()->()")  # offsets
+# def batch_gen_spectrum_numerical(R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offset, dwa, dwb, dwc, power, tp):
+#     dwa *= B0 * gamma
+#     dwb *= B0 * gamma
+#     dwc *= B0 * gamma
+#     offset *= B0 * gamma
+#     power *= gamma
 
-Z_symbolic = batch_gen_spectrum_symbolic(
-    R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offsets, dwa, dwb, dwc, powers, tp
-)
-Z_numerical = batch_gen_spectrum_numerical(
-    R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offsets, dwa, dwb, dwc, powers, tp
-)
+#     kab = fb * kb
+#     kac = fc * kc
+#     L_a = jnp.array([[-R2a, dwa - offset, 0], [offset - dwa, -R2a, power], [0, -power, -R1a]])
+#     L_b = jnp.array([[-R2b, dwb - offset, 0], [offset - dwb, -R2b, power], [0, -power, -R1b]])
+#     L_c = jnp.array([[-R2c, dwc - offset, 0], [offset - dwc, -R2c, power], [0, -power, -R1c]])
+#     K = jnp.array(
+#         [
+#             [-(kab + kac), kb, kc],
+#             [kab, -kb, 0],
+#             [kac, 0, -kc],
+#         ]
+#     )
+#     # Define coefficient matrix of BM equations
+#     A = jax.scipy.linalg.block_diag(L_a, L_b, L_c) + jnp.linalg.tensordot(K, jnp.eye(3))
+#     b = jnp.array([0, 0, R1a, 0, 0, R1b * fb, 0, 0, R1c * fc])
 
-fig, axs = plt.subplots(ncols=2)
-fig.set_figwidth(14)
-axs[0].plot(offsets, Z_symbolic.T)
-axs[1].plot(offsets, Z_numerical.T)
+#     M0 = jnp.array([0, 0, 1, 0, 0, fb, 0, 0, fc])
+#     M_ss = jnp.linalg.solve(A, -b)
+#     Z = ((M0 - M_ss) @ jax.scipy.linalg.expm(A * tp, max_squarings=18) + M_ss)[2]
+#     return Z
+
+
+# R1a = 1 / 3.0
+# R2a = 1 / 2.0
+# dwa = 0.0
+# R1b = 1.0
+# R2b = 30.0
+# R1c = 2.0
+# R2c = 15.0
+# kb = 200.0
+# kc = 30.0
+# fb = 5e-4
+# fc = 1e-3
+# dwb = 3.5
+# dwc = 2.0
+# offsets = jnp.arange(-6, 6, 0.01, dtype=float)
+# powers = jnp.array([1.0, 3.0], dtype=float)
+# B0 = 4.7
+# gamma = 267.522
+# tp = 15.0
+
+# Z_symbolic = batch_gen_spectrum_symbolic(
+#     R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offsets, dwa, dwb, dwc, powers, tp
+# )
+# Z_numerical = batch_gen_spectrum_numerical(
+#     R1a, R2a, R1b, R2b, R1c, R2c, kb, kc, fb, fc, offsets, dwa, dwb, dwc, powers, tp
+# )
+
+# fig, axs = plt.subplots(ncols=2)
+# fig.set_figwidth(14)
+# axs[0].plot(offsets, Z_symbolic.T)
+# axs[1].plot(offsets, Z_numerical.T)
